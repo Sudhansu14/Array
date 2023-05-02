b5cf3d74b34fe201d90fb7ed1eddc5dd0e8d195a
Sudhansu14 <130463585+Sudhansu14@users.noreply.github.com>
2023-05-02 17:48:37 +0530

    Add files via upload

---
+++ Array 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+ 
+    int Arr[] = {1,2,3,4,5};
+   
+    int length = sizeof(Arr) / sizeof(Arr[0]);
+ 
+    printf("The length of the array is: %d\n", length);
+ 
+}
---
+++ Array 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main() 
+{
+    int n,i;
+    float num[10],sum=0,avg;
+
+    printf("Enter the numbers of elements");
+    scanf("%d",&n);
+    
+    for (i=0;i<n;++i) 
+	{
+        printf("%d Enter number",i+1);
+        scanf("%f", &num[i]);
+        sum+=num[i];
+    }
+
+    avg=sum/n;
+    printf("Average=%2f",avg);
+}
+
